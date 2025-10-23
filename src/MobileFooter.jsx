8fc973de3883b5d02e8df1cb660fdd975417f9f3
OdiumMac <davidlancepacheco@gmail.com>
2025-10-23 10:57:38 +0800

    to publish

--- src/MobileFooter.jsx
+++ src/MobileFooter.jsx
@@ -1,6 +1,6 @@
 function MobileFooter(){
     return(
-        <footer class="fixed bottom-0 left-0 w-full text-white text-center py-3 text-[8px]">
+        <footer class="fixed bottom-0 left-0 w-full text-white text-center py-3 text-[9px]">
              Made by Group 4 - Bulfa & Pacheco for design | Mahilum, Flor, Gibaga, Oliveros for research
         </footer>
     )
